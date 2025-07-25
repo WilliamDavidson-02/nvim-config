0d0eb508f3f2d083e1824325a435aa3e03d16696
William Davidson <wj.davidson@outlook.com>
2025-07-25 22:21:42 +0200

    feat: updated keymaps and added git diff view

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -14,8 +14,8 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+-- vim.opt.undodir = os.getenv("home") .. "/.vim/undodir"
+-- vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -23,3 +23,7 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 
 vim.g.mapleader = " "
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -68,7 +68,7 @@ return {
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            lspconfig.tsserver.setup({
+            lspconfig.ts_ls.setup({
                 capabilities = capabilities,
                 on_attach = function(_, bufnr)
                     local opts = { buffer = bufnr, desc = "LSP Hover" }
@@ -127,6 +127,35 @@ return {
     {
         "mg979/vim-visual-multi",
         branch = "master"
-    }
+    },
+
+    {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewFileHistory",
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("diffview").setup({
+        enhanced_diff_hl = true,
+        view = {
+          default = {
+            layout = "diff2_horizontal",
+          },
+        },
+        file_panel = {
+          listing_style = "tree",
+          win_config = {
+            position = "left",
+            width = 35,
+          },
+        },
+      })
+    end,
+  },
 }
 
