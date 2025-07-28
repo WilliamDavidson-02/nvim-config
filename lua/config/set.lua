7fb359a195de5d6fb58daadfab9169c6c1594a99
William Davidson <wj.davidson@outlook.com>
2025-07-28 12:36:53 +0200

    feat: disabled netrew banner

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,4 +1,6 @@
 vim.opt.guicursor = ""
+vim.g.netrw_banner = 0
+vim.g.mapleader = " "
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -14,16 +16,17 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("home") .. "/.vim/undodir"
--- vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
 
-vim.g.mapleader = " "
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
+vim.opt.timeoutlen = 300
+vim.opt.ttimeoutlen = 10
 
