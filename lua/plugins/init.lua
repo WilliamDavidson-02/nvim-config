5fde7d11c6311010370c53e5ca0142c1f31b7c56
William Davidson <wj.davidson@outlook.com>
2025-07-31 17:50:47 +0200

    feat: autotag plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -157,5 +157,15 @@ return {
       })
     end,
   },
+
+  {
+  "windwp/nvim-ts-autotag",
+  event = "InsertEnter",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("nvim-ts-autotag").setup()
+  end,
+  }
+
 }
 
